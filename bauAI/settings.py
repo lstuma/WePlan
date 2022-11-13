347e780efd1727e2d60f2cecf3becacb6da58f60
raspitim <raspitim@users.noreply.github.com>
2022-11-13 11:07:42 +0100

    requirements.txt

--- bauAI/settings.py
+++ bauAI/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = "/var/www/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
